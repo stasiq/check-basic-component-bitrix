102c0887768a4a2ad4c1d8320c385942bc3db701
s.konovalov <s.konovalov@intensa.ru>
2023-05-11 16:19:28 +0300

    Правка

--- check.basic/example.php
+++ check.basic/example.php
@@ -8,7 +8,6 @@ $APPLICATION->IncludeComponent(
         "IBLOCK_TYPE" => "other",
         "IBLOCK_ID" => "1",
         "ELEMENT_COUNT" => "4",
-        "ELEMENT_URL" => "#SITE_DIR#/#IBLOCK_CODE#/item/id/#ELEMENT_ID#/",
         "CACHE_TYPE" => "A",
         "CACHE_TIME" => "3600"
     ),
