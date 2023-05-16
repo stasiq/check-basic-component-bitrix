07d7bb10a8c41ea8bcf89610943162cd44c11af4
s.konovalov <s.konovalov@intensa.ru>
2023-05-16 17:45:22 +0300

    Корректировка кода

--- check.basic/templates/.default/template.php
+++ check.basic/templates/.default/template.php
@@ -1,14 +1,14 @@
 <?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<section id="iblock-random-items">
-    <?php foreach ($arResult['ITEMS'] as $arItem): ?>
-        <article>
-            <h4 <?= $arItem['BASIC_STATUS'] ? 'style="color: red"' : '' ?> ><?= $arItem['NAME']; ?></h4>
-            <p>URL: <?= $arItem['URL'] ?></p>
-        </article>
-    <?php endforeach; ?>
-</section>
+    <section id="iblock-random-items">
+        <?php foreach ($arResult['ITEMS'] as $arItem): ?>
+            <article>
+                <h4 <?= $arItem['BASIC_STATUS'] ? 'style="color: red"' : '' ?> ><?= $arItem['NAME']; ?></h4>
+                <p>URL: <?= $arItem['URL'] ?></p>
+            </article>
+        <?php endforeach; ?>
+    </section>
 
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-    <br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
+<? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
+    <br/><?= $arResult["NAV_STRING"] ?>
+<? endif; ?>
